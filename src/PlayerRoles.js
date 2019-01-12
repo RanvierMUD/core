a62dfe32df9e10a030466a77665a66c56aee2238
Shawn Biddle <shawncplus@gmail.com>
2019-01-11 20:59:28 -0500

    Fix playerroles enum

--- src/PlayerRoles.js
+++ src/PlayerRoles.js
@@ -1,14 +1,12 @@
 'use strict';
 
 
-/** @module PlayerRoles */
+/**
+ * @module PlayerRoles
+ * @enum {number}
+ */
 module.exports = {
-  /**
-   * @enum {number}
-   */
-  PlayerRoles: {
-    ADMIN: 2,
-    BUILDER: 1,
-    PLAYER: 0
-  }
+  ADMIN: 2,
+  BUILDER: 1,
+  PLAYER: 0
 };
