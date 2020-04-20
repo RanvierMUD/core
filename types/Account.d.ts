e1a93e3c6b7b228c6daf1df122e9949377fb46ab
Claudio Giordano <claudio.giordano@autistici.org>
2020-04-20 23:22:10 +0200

    Prettier fix

--- types/Account.d.ts
+++ types/Account.d.ts
@@ -1,87 +1,87 @@
 export declare interface AccountConfig {
-    /** @property {string} username */
-    username: string;
-    /** @property {Array<string>} characters List of character names in this account */
-    characters: Array<string>;
-    /** @property {string} password Hashed password */
-    password: string;
-    /** @property {boolean} banned Whether this account is banned or not */
-    banned: boolean;
+  /** @property {string} username */
+  username: string;
+  /** @property {Array<string>} characters List of character names in this account */
+  characters: Array<string>;
+  /** @property {string} password Hashed password */
+  password: string;
+  /** @property {boolean} banned Whether this account is banned or not */
+  banned: boolean;
 }
 
 export declare class Account {
-    /** @property {string} username */
-    username: string;
-    /** @property {Array<string>} characters List of character names in this account */
-    characters: Array<string>;
-    /** @property {string} password Hashed password */
-    password: string;
-    /** @property {boolean} banned Whether this account is banned or not */
-    banned: boolean;
+  /** @property {string} username */
+  username: string;
+  /** @property {Array<string>} characters List of character names in this account */
+  characters: Array<string>;
+  /** @property {string} password Hashed password */
+  password: string;
+  /** @property {boolean} banned Whether this account is banned or not */
+  banned: boolean;
 
-    /**
-     * @param {AccountConfig} data Account save data
-     */
-    constructor(data: AccountConfig);
+  /**
+   * @param {AccountConfig} data Account save data
+   */
+  constructor(data: AccountConfig);
 
-    /**
-     * @return {string}
-     */
-    getUsername(): string;
+  /**
+   * @return {string}
+   */
+  getUsername(): string;
 
-    /**
-     * @param {string} username
-     */
-    addCharacter(username: string): void;
+  /**
+   * @param {string} username
+   */
+  addCharacter(username: string): void;
 
-    /**
-     * @param {string} name
-     * @return {boolean}
-     */
-    hasCharacter(name: string): boolean;
+  /**
+   * @param {string} name
+   * @return {boolean}
+   */
+  hasCharacter(name: string): boolean;
 
-    /**
-     * @param {string} name Delete one of the chars
-     */
-    deleteCharacter(name: string): void;
+  /**
+   * @param {string} name Delete one of the chars
+   */
+  deleteCharacter(name: string): void;
 
-    /**
-     * @param {string} name Removes the deletion of one of the chars
-     */
-    undeleteCharacter(name: string): void;
+  /**
+   * @param {string} name Removes the deletion of one of the chars
+   */
+  undeleteCharacter(name: string): void;
 
-    /**
-     * @param {string} password Unhashed password. Is hashed inside this function
-     */
-    setPassword(pass: string): void;
+  /**
+   * @param {string} password Unhashed password. Is hashed inside this function
+   */
+  setPassword(pass: string): void;
 
-    /**
-     * @param {string} pass Unhashed password to check against account's password
-     * @return {boolean}
-     */
-    checkPassword(pass: string): boolean;
+  /**
+   * @param {string} pass Unhashed password to check against account's password
+   * @return {boolean}
+   */
+  checkPassword(pass: string): boolean;
 
-    /**
-     * @param {function} callback after-save callback
-     */
-    save(callback: Function): void;
+  /**
+   * @param {function} callback after-save callback
+   */
+  save(callback: Function): void;
 
-    /**
+  /**
      * Set this account to banned
         There is no unban because this can just be done by manually editing the account file
     */
-    ban(): void;
+  ban(): void;
 
-    /**
+  /**
      * Set this account to deleted
      There is no undelete because this can just be done by manually editing the account file
     */
-    deleteAccount(): void;
+  deleteAccount(): void;
 
-    /**
-     * Gather data from account object that will be persisted to disk
-     *
-     * @return {Object}
-     */
-    serialize(): Object;
+  /**
+   * Gather data from account object that will be persisted to disk
+   *
+   * @return {Object}
+   */
+  serialize(): Object;
 }
