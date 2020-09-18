e651b3b2446001da2023880cfa4f72aa8f83f1a0
Sean O'Donohue <sean.odonohue@detroitlabs.com>
2020-09-18 18:59:58 -0400

    Fallback should be optional

--- types/Config.d.ts
+++ types/Config.d.ts
@@ -3,7 +3,7 @@ export declare class Config {
      * @param {string} key
      * @param {*} fallback fallback value
      */
-    static get(key: string, fallback: any): any;
+    static get(key: string, fallback?: any): any;
 
     /**
      * Load `ranvier.json` from disk
