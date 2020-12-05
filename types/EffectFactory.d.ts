c521b78e912f60c4eca11b6c7990553bed205deb
Claudio Giordano <claudio.giordano@autistici.org>
2020-12-05 23:00:41 +0100

    Add definitions for EffectConfig (type used by effect factory) and EffectFactory

---
+++ types/EffectFactory.d.ts
@@ -0,0 +1,38 @@
+import { Effect } from "./Effect";
+import { GameState } from "./GameState";
+
+export declare type EffectConfig = {
+    config: Object<string, any>;
+    listeners: Object<string, Function>;
+}
+
+export declare class EffectFactory {
+    constructor(): EffectFactory;
+
+    /**
+     * @param {string} id
+     * @param {EffectConfig} config
+     * @param {GameState} state
+     */
+    add(id: string, config: EffectConfig, state: GameState): void;
+
+    /**
+     * @param {string} id
+     */
+    has(id: string): boolean;
+
+    /**
+     * Get a effect definition. Use `create` if you want an instance of a effect
+     * @param {string} id
+     * @return {object}
+     */
+    get(id: string): Effect;
+
+    /**
+     * @param {string}  id      effect id
+     * @param {?object} config  Effect.config override
+     * @param {?object} state   Effect.state override
+     * @return {Effect}
+     */
+    create(id: string, config?: EffectConfig, state: object): Effect
+}
