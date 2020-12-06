ed5b9f8762ba65b793ad76e981c0359588133d64
Claudio Giordano <claudio.giordano@autistici.org>
2020-12-06 21:27:14 +0100

    Add definition for QuestFactory

--- types/QuestFactory.d.ts
+++ types/QuestFactory.d.ts
@@ -1,3 +1,47 @@
-export declare class QuestFactory {
+import { GameState } from './GameState';
+import { Player } from './Player';
+import { Quest, QuestConfig } from './Quest';
 
+export declare class QuestFactory {
+  constructor();
+
+  add(areaName: string, id: string, config: QuestConfig);
+
+  set(qid: string, val: QuestConfig);
+
+  /**
+   * Get a quest definition. Use `create` if you want an instance of a quest
+   * @param {string} qid
+   * @return {object}
+   */
+  get(qid: string): Quest;
+
+  /**
+   * Check to see if a player can start a given quest based on the quest's
+   * prerequisite quests
+   * @param {entityReference} questRef
+   * @return {boolean}
+   */
+  canStart(player: Player, questRef: string);
+
+  /**
+   * @param {GameState} GameState
+   * @param {entityReference} qid
+   * @param {Player}    player
+   * @param {Array}     state     current quest state
+   * @return {Quest}
+   */
+  create(
+    GameState: GameState,
+    qid: string,
+    player: Player,
+    state: any[]
+  ): Quest;
+
+  /**
+   * @param {string} areaName
+   * @param {number} id
+   * @return {string}
+   */
+  makeQuestKey(area: string, id: number): string;
 }
